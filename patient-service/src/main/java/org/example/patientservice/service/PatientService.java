c3177cc08df9863949e2a5a706df7b0e2282ec93
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-04-30 10:26:28 +1000

    first-commit

---
+++ patient-service/src/main/java/org/example/patientservice/service/PatientService.java
@@ -0,0 +1,25 @@
+package org.example.patientservice.service;
+
+
+import org.example.patientservice.dto.PatientResponseDTO;
+import org.example.patientservice.model.Patient;
+import org.example.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository=patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients=patientRepository.findAll();
+        return List.of();
+    }
+
+}
